28e967a42494668cb5b0e3f040f7ff2c8bda06a9
davidemontagna <davidemontagna92@gmail.com>
2022-01-17 15:05:59 +0100

    repo creata

---
+++ main.js
@@ -0,0 +1,39 @@
+let app = new Vue({
+    el: "#app",
+    data: {
+        
+        elements: [
+            {
+                text: 'Monopoly',
+                done: false
+            },
+            {
+                text: 'Vudulhu',
+                done: false
+            },
+            {
+                text: 'Terraforming Mars',
+                done: false
+            },
+            {
+                text: 'Puerto Rico',
+                done: false
+            },
+            {
+                text: 'Dixit',
+                done: false
+            },
+            {
+                text: 'I Coloni di Catan',
+                done: false
+            },
+            {
+                text: 'Sherlock Holmes Consulente Investigativo: I Delitti del Tamigi',
+                done: false
+            },
+        ]
+    },
+    methods: {
+
+    }
+})
